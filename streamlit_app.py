319346860bef9fc4525b3e06c26dad64ce5030cf
raudhatulhanna <hannaraudhatul12@gmail.com>
2026-05-22 14:21:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.title("🎈 Projek Kelas A")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
 import streamlit as st
 
 st.markdown("*Streamlit* is **really** ***cool***.")
